b92c01e77140bd8981690f014a875fa15ce1182d
marquini76 <derekm2@uci.edu>
2021-04-27 15:42:21 -0700

    Removed user agent, added back middleware

--- YahooCrawler/YahooCrawler/settings.py
+++ YahooCrawler/YahooCrawler/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'YahooCrawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
+#USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -52,7 +52,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     'YahooCrawler.middlewares.YahoocrawlerDownloaderMiddleware': None,
-    'YahooCrawler.middlewares.SeleniumDownloaderMiddleware': None, #543, commented out for now
+    'YahooCrawler.middlewares.SeleniumDownloaderMiddleware': 543, #543, commented out for now
 }
 
 # Enable or disable extensions
