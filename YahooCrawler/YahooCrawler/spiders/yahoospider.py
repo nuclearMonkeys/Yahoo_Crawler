87611d909cb9e640d3a2a994f901e4d61a2d0f4d
marquini76 <derekm2@uci.edu>
2021-04-27 22:01:16 -0700

    Mostly working crawler, minus answers
    As well as mass crawling across Yahoo Answers. Hopefully to be done tomorrow.

--- YahooCrawler/YahooCrawler/spiders/yahoospider.py
+++ YahooCrawler/YahooCrawler/spiders/yahoospider.py
@@ -10,38 +10,14 @@ class YahooSpider(scrapy.Spider):
     name = 'Battery'
     allowed_domains = ['answers.yahoo.com']
     start_urls = ['https://answers.yahoo.com/']
-    #headers = {
-    #    "authority": "answers.yahoo.com",
-    #    "method": "PUT",
-    #    "path": "/_reservice_/",
-    #    "scheme": "https",
-    #    "accept": "application/json",
-    #    "accept-encoding": "gzip, deflate, br",
-    #    "accept-language": "en-US,en;q=0.9",
-    #    "origin": "https://answers.yahoo.com",
-    #    "referer": "https://answers.yahoo.com/",
-    #    "sec-fetch-dest": "empty",
-    #    "sec-fetch-mode": "cors",
-    #    "sec-fetch-site": "same-origin",
-    #    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36",
-    #}
 
     def parse(self, response):
         frontitem = YahoocrawlerItem()
-        for question in response.css('div.QuestionCard__questionCard___18EIQ'):
-            #itemload = ItemLoader(frontitem = YahoocrawlerItem(), selector = question)
-            #itemload.add_css('title', 'a.QuestionCard__title___1DKC-')
-            #itemload.add_css('numofanswers', 'span.QuestionInfo__answersCount___1gokz')
-            #itemload.add_css('category', 'a.QuestionInfo__category___2kFc4')
-            #itemload.add_value('link', urljoin('https://answers.yahoo.com', question.css('a.QuestionCard__title___1DKC-').attrib['href']))
-
-            frontitem['title'] = question.css('a.QuestionCard__title___1DKC-::text').get()
-            frontitem['numofanswers'] = question.css('span.QuestionInfo__answersCount___1gokz::text').get()
-            frontitem['category'] = question.css('a.QuestionInfo__category___2kFc4::text').get()
+        for question in response.css('li.CategoryStreamsList__streamItem___2Jgqs.CategoryStreamsList__discoverStreamItem___G2MIg'):
             current_link = urljoin('https://answers.yahoo.com', question.css('a.QuestionCard__title___1DKC-').attrib['href'])
-            frontitem['link'] = current_link
-
-            yield frontitem
+            params = [question.css('a.QuestionCard__title___1DKC-::text').get(), question.css('span.QuestionInfo__answersCount___1gokz::text').get(), question.css('div::text').get(), current_link]
+            # Title, numofanswers, link, upvotes
+            yield response.follow(current_link, callback = self.parse_answers, meta = {'olditem': frontitem, 'params': params})
 
         #for category in response.css('li.CategoryNav__item___11rAo'):
             #next_page = urljoin('https://answers.yahoo.com', category.css('a').attrib['href'])
@@ -53,12 +29,36 @@ class YahooSpider(scrapy.Spider):
         for question in response.css('div.QuestionCard__questionCard___18EIQ'):
             item['title'] = question.css('a.QuestionCard__title___1DKC-::text').get()
             item['numofanswers'] = question.css('span.QuestionInfo__answersCount___1gokz::text').get()
-            item['category'] = question.css('a.QuestionInfo__category___2kFc4::text').get()
             quest_link = urljoin('https://answers.yahoo.com', question.css('a.QuestionCard__title___1DKC-').attrib['href'])
             item['link'] = quest_link
-
+            yield response.follow(quest_link, callback = self.parse_answers, meta={'olditem': item})
             yield item
+
+    def parse_subcategories(self, response):
+        # For parsing through the subcategories of individual categories    
+        pass
 
     def parse_answers(self, response):
         # For parsing through a question's answers
-        pass
+        olditem = response.meta.get('olditem')
+        params = response.meta.get('params')
+        olditem['title'] = params[0]
+        olditem['numofanswers'] = params[1]
+        olditem['upvotes'] = params[2]
+        olditem['link'] = params[3]
+        olditem['user'] = response.xpath('/html/body/div/div[1]/div/div/main/div/div[3]/div[1]/div[1]/div/div[1]/div[2]/a/text()').get()
+        userlink = urljoin('https://answers.yahoo.com', response.xpath('/html/body/div/div[1]/div/div/main/div/div[3]/div[1]/div[1]/div/div[1]/div[2]/a/@href'))
+        olditem['userpage'] = userlink
+        olditem['questiontext'] = response.xpath('/html/body/div/div[1]/div/div/main/div/div[3]/div[1]/div[2]/div/p/text()').get()
+        olditem['category'] = response.xpath('/html/body/div/div[1]/div/div/main/div/div[3]/div[1]/div[1]/div/div[2]/div/a[2]/text()').get()
+        all_answers = list()
+        for answer in response.css('ul.AnswersList__answersList___2ikkB'):
+            user = dict()
+            fullanswer = answer.css('p::text').getall()
+            user['answer'] = ' '.join(fullanswer)
+            user['user'] = answer.css('a.UserProfile__userName___1d1RW::text')
+            user['userpage'] = urljoin('https://answers.yahoo.com', answer.css('a.UserProfile__userName___1d1RW').attrib['href'])
+            user['likes'] = 
+            user['dislikes'] =
+            all_answers.append(user)
+        yield olditem
