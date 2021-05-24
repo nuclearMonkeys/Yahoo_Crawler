a46cd9b3f1882e94399fd56cc01305a6818ec45a
nuclearMonkeys <33884355+nuclearMonkeys@users.noreply.github.com>
2021-05-23 17:10:54 -0700

    Actual start of the frontend website

---
+++ search_linker.js
@@ -0,0 +1,14 @@
+// const f = document.getElementById('form');
+// const q = document.getElementById('query');
+// const google = 'https://www.google.com/search?q=site%3A+';
+// const site = 'pagedart.com';
+
+// var mysql = require('mysql');
+
+// var connection = mysql.createConnection({
+    
+// })
+
+var options = {
+    host: 'private-github-api.com'
+}
