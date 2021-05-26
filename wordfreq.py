b9393e10d3c5ab3588eb5f9ac625d0bd9772e3d4
marquini76 <derekm2@uci.edu>
2021-05-25 23:41:05 -0700

    Complete word freq and postings list creator

--- wordfreq.py
+++ wordfreq.py
@@ -102,19 +102,37 @@ def json_reading_post(crawl_test, crawl_dict):
     # Reading the json files with the crawl information for the purposes of
     # creating a postings list for easier access to a certain question with
     # a specific word. In theory, should make searches easier.
+    temp_dict = dict()
     for crawl in crawl_test:
         print(crawl)
-        with open(crawl, encoding='utf-8', errors='ignore') as crawl_data:
-            YahooObject = json.loads(crawl_data.read())
+        with open(crawl, encoding='utf-8', errors='ignore') as crawl_data2:
+            YahooObject2 = json.loads(crawl_data2.read())
+            for entries in YahooObject2:
+                if check_if_question(entries):
+                    q_title2 = entries.get("title")
+                    try:
+                        print(q_title2)
+                    except:
+                        continue
+                    token_list2 = tokenizeAndLemmatize(q_title2)
+                    quest_id = entries.get('link')[45:]
+                    for word in token_list2:
+                        if temp_dict.get(word) != None:
+                            temp_dict[word].add(quest_id)
+                        else:
+                            temp_dict[word] = {quest_id}
+    
+    for key in sorted(temp_dict):
+        crawl_dict[key] = list(temp_dict[key])
 
 if __name__ == "__main__":
     crawl_test = ["test_crawl15.json", "test_crawl16.json", "test_crawl17.json"]
     crawl_list = list()
     crawl_dict = dict()
-    with open("word_frequency.json", "w") as outfile:
-        json_reading_freq(crawl_test, crawl_list)
-        json_list = json.dumps(crawl_list)
-        outfile.write(json_list)
+    #with open("word_frequency.json", "w") as outfile:
+    #    json_reading_freq(crawl_test, crawl_list)
+    #    json_list = json.dumps(crawl_list)
+    #    outfile.write(json_list)
     with open("Yahoo_postingslist.json", "w") as outfile2:
         json_reading_post(crawl_test, crawl_dict)
         json_dict = json.dumps(crawl_dict)
